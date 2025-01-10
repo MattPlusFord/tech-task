750b75f2e8524f982dcc653ef4e9732a5c96f435
Matt Nicks <mnicks@ford.com>
2025-01-10 15:08:47 +0000

    feat(US0000000): add a basic landing page ui

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,17 @@
 import Header from "./components/layout/header.tsx";
 import './components/layout/layout.css';
+import './App.css';
+import HomePage from "./pages/home.tsx";
+import Footer from "./components/layout/footer.tsx";
 
 export const App = () => {
     return (
         <>
             <Header />
-            <h2>Tech Interview</h2>
+            <main className="main">
+                <HomePage />
+            </main>
+            <Footer />
         </>
     )
 }
---
+++ ui/src/components/cards/card.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import './cards.css';
+
+interface CardProps {
+    children: ReactNode;
+}
+
+export function Card({ children }: CardProps) {
+    return <div className={'card'}>{children}</div>;
+}
