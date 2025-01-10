af6e1d1d73f39f189f294e6d79a5f3e62f774759
Matt Nicks <mnicks@ford.com>
2025-01-10 14:23:41 +0000

    init ui

---
+++ ui/src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/layout/header.tsx";
+import './components/layout/layout.css';
+
+export const App = () => {
+    return (
+        <>
+            <Header />
+            <h2>Tech Interview</h2>
+        </>
+    )
+}
+
+export default App;
---
+++ ui/src/components/layout/header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+    return (
+        <div className="header">
+            <a href={'/'} className={'home-icon'}>Ford Credit</a>
+        </div>
+    )
+}
+
+export default Header;
